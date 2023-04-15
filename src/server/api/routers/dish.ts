c9d6d4308f7b41d68917e228580dab8f796bb729
Joshua Völkerink <joshua.voelkerink@gmx.de>
2023-04-15 22:41:44 +0200

    quick save

--- src/server/api/routers/dish.ts
+++ src/server/api/routers/dish.ts
@@ -3,7 +3,10 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const dishRouter = createTRPCRouter({
-  getAllDishes: publicProcedure.query(({ ctx }) => {
+  getAllDishes: publicProcedure.query(async ({ ctx }) => {
+    const dishes = await ctx.prisma.dish.findMany({
+      take: 8,
+    })
     return ctx.prisma.dish.findMany();
   }),
 });
